b42bda09b46260cc256b9461637ad75a058a9ce6
Garrett Miller <gmiller@slack-corp.com>
2016-01-19 16:59:20 -0800

    Urban Dictionary basic slash command

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 /* Uses the slack button feature to offer a real time bot to multiple teams */
 var Botkit = require('botkit');
+var urban = require('urban');
 
 if (!process.env.clientId || !process.env.clientSecret || !process.env.PORT) {
   console.log('Error: Specify clientId clientSecret and port in environment');
@@ -22,7 +23,7 @@ var controller = Botkit.slackbot(config).configureSlackApp(
   {
     clientId: process.env.clientId,
     clientSecret: process.env.clientSecret,
-    scopes: ['bot'],
+    scopes: ['commands'],
   }
 );
 
@@ -38,88 +39,30 @@ controller.setupWebserver(process.env.PORT,function(err,webserver) {
   });
 });
 
+controller.on('slash_command',function(bot,message) {
+  var searchTerm = message.text;
+  var botMessage = '';
+  var ret;
 
-// just a simple way to make sure we don't
-// connect to the RTM twice for the same team
-var _bots = {};
-function trackBot(bot) {
-  _bots[bot.config.token] = bot;
-}
-
-controller.on('create_bot',function(bot,config) {
-
-  if (_bots[bot.config.token]) {
-    // already online! do nothing.
+  if(!searchTerm) {
+    botMessage = ":confused: Sorry, I need to know what to search for. Since you _didn't_ give me anything to work with, here's a random definition!\n\n";
+    ret = urban.random();
   } else {
-    bot.startRTM(function(err) {
+    ret = new urban(searchTerm);
+  }
 
-      if (!err) {
-        trackBot(bot);
-      }
+  ret.first(function( item ) {
 
-      bot.startPrivateConversation({user: config.createdBy},function(err,convo) {
-        if (err) {
-          console.log(err);
-        } else {
-          convo.say('I am a bot that has just joined your team');
-          convo.say('You must now /invite me to a channel so that I can be of use!');
-        }
-      });
-
+    bot.replyPrivate( message, {
+        text: botMessage,
+        attachments:  [{
+          'title': item.word,
+          'title_link': item.permalink,
+          "text": item.definition,
+          "color": "#0748EA"
+        }],
     });
-  }
 
-});
-
-
-// Handle events related to the websocket connection to Slack
-controller.on('rtm_open',function(bot) {
-  console.log('** The RTM api just connected!');
-});
-
-controller.on('rtm_close',function(bot) {
-  console.log('** The RTM api just closed');
-  // you may want to attempt to re-open
-});
-
-controller.hears('hello','direct_message',function(bot,message) {
-  bot.reply(message,'Hello!');
-});
-
-controller.hears('^stop','direct_message',function(bot,message) {
-  bot.reply(message,'Goodbye');
-  bot.rtm.close();
-});
-
-controller.on('direct_message,mention,direct_mention',function(bot,message) {
-  bot.api.reactions.add({
-    timestamp: message.ts,
-    channel: message.channel,
-    name: 'robot_face',
-  },function(err) {
-    if (err) { console.log(err) }
-    bot.reply(message,'I heard you loud and clear boss.');
   });
-});
-
-controller.storage.teams.all(function(err,teams) {
-
-  if (err) {
-    throw new Error(err);
-  }
-
-  // connect all teams with bots up to slack!
-  for (var t  in teams) {
-    if (teams[t].bot) {
-      var bot = controller.spawn(teams[t]).startRTM(function(err) {
-        if (err) {
-          console.log('Error connecting bot to Slack:',err);
-        } else {
-          trackBot(bot);
-        }
-      });
-    }
-  }
 
 });
-
